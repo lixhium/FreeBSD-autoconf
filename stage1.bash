937961aaa7050ba714097611e24ae0672594aee8
lixhium <notkoishi@gmail.com>
2023-12-28 22:28:08 +0800

    mega update idk

--- stage1.bash
+++ stage1.bash
@@ -7,6 +7,10 @@ echo "installing... idk software that I need lol"
 pkg install -y doas neovim git wget curl xorg drm-kmod xf86-video-amdgpu jetbrains-mono nerd-fonts \
     pulse-audio pavucontrol npm llvm alacritty firefox
 
+echo "setting up the har har har"
+echo "kld_list=\"amdgpu\"" >> /etc/rc.conf
+echo "dbus_enable=\"YES\"" >> /etc/rc.conf
+
 echo "setting up doas"
 echo "permit :wheel" >> /usr/local/etc/doas.conf
 

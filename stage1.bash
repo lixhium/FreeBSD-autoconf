f294f1214d71f9c6daf922ae3fb9b32c0d2d6d9e
lixhium <notkoishi@gmail.com>
2023-12-28 22:45:51 +0800

    added sound

--- stage1.bash
+++ stage1.bash
@@ -4,10 +4,12 @@ echo "please... PLEASE JUST TYPE IN \"y\""
 pkg
 
 echo "installing... idk software that I need lol"
-pkg install -y doas neovim git wget curl xorg drm-kmod xf86-video-amdgpu jetbrains-mono nerd-fonts \
+pkg install -y doas neovim wget curl xorg drm-kmod xf86-video-amdgpu jetbrains-mono nerd-fonts \
     pulse-audio pavucontrol npm llvm alacritty firefox
 
 echo "setting up the har har har"
+kldload snd_driver
+echo "hw.snd.default_unit=4" >> /etc/sysctl.conf
 echo "kld_list=\"amdgpu\"" >> /etc/rc.conf
 echo "dbus_enable=\"YES\"" >> /etc/rc.conf
 
